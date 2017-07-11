bc7ed226a5c71c630d78c421bbf707ccd51f7f84
Serjo96 <king-beer@yandex.ru>
2017-07-12 03:34:20 +0500

    add autocompite

---
+++ src/index.js
@@ -0,0 +1,26 @@
+/* ДЗ 6.1 - Асинхронность и работа с сетью */
+/**
+ * Функция должна создавать Promise, который должен быть resolved через seconds секунду после создания
+ *
+ * @param {number} seconds - количество секунд, через которое Promise должен быть resolved
+ * @return {Promise}
+ */
+function delayPromise(seconds) {
+    var p = new Promise(function(resolve, reject) {
+        setTimeout(function() { resolve() }, seconds * 1000);
+    });
+  return  p.then(function() {});
+}
+
+/**
+ * Функция должна вернуть Promise, который должен быть разрешен массивом городов, загруженным из
+ * https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json
+ * Элементы полученного массива должны быть отсортированы по имени города
+ *
+ * @return {Promise<Array<{name: String}>>}
+ */
+
+
+export {
+    delayPromise
+};
